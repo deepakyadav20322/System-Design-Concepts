9e733d1f33f2bec9a02772af099456dcb7f7423d
deepak <informationtechnology20322@gmail.com>
2025-02-21 13:51:18 +0530

    Initialize the project

---
+++ nested-comment/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+function App() {
+
+  return (
+    <div className='text-center w-full py-6'>
+      <p>Welcome</p>
+      This is starting point of nested comment system design question
+    </div>
+  )
+}
+
+export default App
